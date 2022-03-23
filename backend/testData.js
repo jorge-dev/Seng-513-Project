643da1398cf0ea1581990382d31e77524a46c855
jorgeavilaBL <javila@blacklinesafety.com>
2022-03-23 16:48:12 -0600

    added test data for testing db calls

---
+++ backend/testData.js
@@ -0,0 +1,29 @@
+const data = {
+  test: [
+    {
+      name: "Bicycle",
+      message: "This is a bicycle",
+      image:
+        "https://res.cloudinary.com/cloud-513/image/upload/v1647884527/samples/people/bicycle.jpg",
+    },
+    {
+      name: "Snow Baby",
+      message: "This is a baby enjoying the snow",
+      image:
+        "https://res.cloudinary.com/cloud-513/image/upload/v1647884526/samples/people/boy-snow-hoodie.jpg",
+    },
+    {
+      name: "Jazz BAnd",
+      message: "This is a Jazz band performing",
+      image:
+        "https://res.cloudinary.com/cloud-513/image/upload/v1647884526/samples/people/jazz.jpg",
+    },
+    {
+      name: "Null",
+      message: "Null test",
+      image: "img.png",
+    },
+  ],
+};
+
+export default data;
