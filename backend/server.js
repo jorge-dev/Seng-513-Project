12a6f5065314e234f737840dd0e8e6708c2d0571
jorge-dev <jo.luisa94@gmail.com>
2022-03-23 21:52:21 -0600

    Added .env-template file and fixed env variable name

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_DB_URl, {
+  .connect(process.env.MONGO_DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
