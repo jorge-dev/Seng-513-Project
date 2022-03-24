cab7a668debef1794dbcc2aeb49d7d7a0ab5f57a
Daryl Dang <daryl.dang4@gmail.com>
2022-03-24 11:32:19 -0600

    Add initial product page commit
    Testing out react.

---
+++ frontend/src/Product.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Product.css';
+
+export default function Product() {
+  return (
+      // TODO: add navbar as div before product
+      <>
+        <div className='Product'>
+            <body>
+                <h1 id='Product-Name-Label'> Product name </h1>
+            </body>
+        </div>
+      </>
+  )
+}
