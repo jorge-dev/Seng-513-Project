08e366ea5346602901bea42125faf5430893ab11
elnasrianconcept <elnasrianconcept@gmail.com>
2022-03-25 17:15:17 -0600

    Template login and creation pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './pages/Login'
+import CreateAccount from './pages/CreateAccount'
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Routes>
+    <Route path="" element={<Login/>}/>
+    <Route path="pages/CreateAccount" element={<CreateAccount/>}/>
+    <Route path="*" element={<h2>Error - page not found</h2>}/>
+  </Routes>
   );
 }
 
