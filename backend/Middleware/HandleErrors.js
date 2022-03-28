1d0d7b0ae09ea5a3d5d712e0e86f9528da9c3fc4
jorge-dev <jo.luisa94@gmail.com>
2022-03-27 18:35:28 -0600

    added error handler functions

---
+++ backend/Middleware/HandleErrors.js
@@ -0,0 +1,16 @@
+const notFoundError = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === "production" ? "🥞" : error.stack,
+  });
+};
+
+export { notFoundError, errorHandler };
