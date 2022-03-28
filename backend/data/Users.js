60a01a1eba156322bcd1e18e60a35cbbb9283e75
jorge-dev <jo.luisa94@gmail.com>
2022-03-27 21:53:20 -0600

    made several changes the userschema

--- backend/data/Users.js
+++ backend/data/Users.js
@@ -3,11 +3,13 @@ const users = [
   {
     name: "John Doe",
     username: "johndoe",
+    email: "johnDoe@email.com",
     password: bcrypt.hashSync("password", 10),
   },
   {
     name: "Admin",
     username: "admin",
+    email: "admin@email.com",
     password: bcrypt.hashSync("password", 10),
     isAdmin: true,
   },
--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 import logger from "../logger/devLogger.js";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
+    name: { type: String, required: false },
     username: { type: String, required: true, unique: true },
+    email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, required: true, default: false },
   },
@@ -12,6 +14,26 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+// Login method
+userSchema.methods.matchPassword = async function (pwd) {
+  return await bcrypt.compare(pwd, this.password);
+};
+
+// Register
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    logger.error(error);
+    next(error);
+  }
+});
 
 const User = mongoose.model("User", userSchema);
 
