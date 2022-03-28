522f2d49d99f60ae40177b3f6cd8f263305dd52d
jorge-dev <jo.luisa94@gmail.com>
2022-03-28 03:06:01 -0600

    Re-engineered orders to better match our purpose

--- backend/models/OrderModel.js
+++ backend/models/OrderModel.js
@@ -2,15 +2,38 @@ import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema(
   {
-    number: { type: Number, required: true, unique: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    products: [
-      { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: [
+      {
+        name: { type: String, required: true },
+        slug: { type: String, required: true },
+        price: { type: Number, required: true },
+        quantity: { type: Number, required: true },
+        image: { type: String, required: true },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+      },
     ],
-    shippingAddress: { type: String, required: true },
+    shippingAddress: {
+      fullName: { type: String, required: true },
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
+    },
+    paymentMethod: { type: String, required: true },
+    paymentStatus: { type: String, required: true, default: "Pending" },
+    itemsPrice: { type: Number, required: true },
+    shippingPrice: { type: Number, required: true },
+    taxPrice: { type: Number, required: true },
     totalPrice: { type: Number, required: true },
-    status: { type: String, required: true, default: "pending" },
-    orderedAt: { type: Date, required: true, default: Date.now },
   },
   {
     timestamps: true,
