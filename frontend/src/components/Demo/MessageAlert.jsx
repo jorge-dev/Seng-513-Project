5dd09be0887a8fdbc6237c52cf2ab9015b7fc078
jorge-dev <jo.luisa94@gmail.com>
2022-03-31 12:11:30 -0600

    fix the message alert component

--- frontend/src/components/Demo/MessageAlert.jsx
+++ frontend/src/components/Demo/MessageAlert.jsx
@@ -2,7 +2,8 @@ import { Alert } from "react-bootstrap";
 
 export default function MessageAlert(props) {
     return (
-        <Alert variant={props.variant || 'info'}> {props.children} </Alert>
+
+        <Alert className="text-center col-sm-12 mx-auto w-50" style={{ marginTop: '10em' }} variant={props.variant || 'info'}> {props.children} </Alert>
 
     )
 }
