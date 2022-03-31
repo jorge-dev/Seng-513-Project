1fc37ef79f6f16fafa7e362ad0ae1d1cf5985173
jorge-dev <jo.luisa94@gmail.com>
2022-03-31 12:10:31 -0600

    added context sotre to app to pass props thoght the whole tree

---
+++ frontend/src/ContextStore.js
@@ -0,0 +1,40 @@
+import { createContext, useReducer } from "react";
+
+export const ContextStore = createContext();
+
+const initialState = {
+    cart: {
+        items: [],
+    }
+};
+
+const reducer = (state, action) => {
+    // switch (action.type) {
+    //     case "ADD_TO_CART":
+    //         const newItem = action.payload;
+    //         // avoid duplicates
+    //         const existingItem = state.cart.items.find(itemInCart => itemInCart._id === newItem._id);
+    //         const cartItems = existingItem ? state.cart.items.map(item =>
+    //             item._id === existingItem._id ? newItem : item) : [...state.cart.items, newItem];
+    //         return { ...state, cart: { ...state.cart, cartItems } };
+
+    switch (action.type) {
+        case "ADD_TO_CART":
+            return {
+                ...state,
+                cart: {
+                    ...state.cart,
+                    items: [...state.cart.items, action.payload]
+                }
+            };
+        default:
+            return state;
+    }
+}
+
+export function ContextStoreProvider(props) {
+    const [state, setState] = useReducer(reducer, initialState);
+    const value = { state, setState };
+    return <ContextStore.Provider value={value}> {props.children} </ContextStore.Provider>;
+
+}
