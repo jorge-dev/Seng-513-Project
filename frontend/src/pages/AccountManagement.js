3869509b67b82eff4bc5f9bd6539b4ed0647455c
jorge-dev <jo.luisa94@gmail.com>
2022-04-01 17:52:40 -0600

    Added context store to AccountManagement.js -- if something doesnt work revert this

--- frontend/src/pages/AccountManagement.js
+++ frontend/src/pages/AccountManagement.js
@@ -1,90 +1,95 @@
 //This code is heavily inspired (but not verbatim copied) from: https://github.com/mui/material-ui/blob/v5.5.2/docs/data/material/getting-started/templates/sign-in/SignIn.js
 
-import React, {useEffect, useState} from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import React, {useContext, useEffect, useState} from "react";
+import {Navigate, useNavigate} from "react-router-dom";
 import {
-    Button,
-    TextField,
+    Alert,
     Box,
-    Typography,
-    Container,
-    Grid,
     Card,
     CardContent,
+    Container,
+    Grid,
+    IconButton,
     List,
     ListItem,
-    Alert
+    TextField,
+    Typography
 } from "@mui/material";
 import EditIcon from '@mui/icons-material/Edit';
-import { grey } from '@mui/material/colors';
-import { IconButton } from "@mui/material"
+import {grey} from '@mui/material/colors';
 
 import axios from "axios";
+import {ContextStore} from "../ContextStore";
 
-export default function AccountManagement()
-{
+export default function AccountManagement() {
     const [lastchanged, lcupdate] = useState("");
     const [orders, setOrders] = useState([]);
+    const {state,} = useContext(ContextStore)
+    const {userInfo} = state
 
     let navigate = useNavigate();
 
-    function price(num)
-    {
+    function price(num) {
         return "$" + num?.toFixed(2);
     }
 
     useEffect(() => {
-        axios.get("/api/orders/user", {"headers": {"Authorization": "Bearer " + userData.token}})
-        .then((response) => {
-            console.log(response.data.orders);
-            response.data.orders.forEach(o => {
-            var n = {
-            id: o._id.toUpperCase(),
-            cost: o.totalPrice,
-            timestamp: "Purchased on: " + new Date(o.createdAt).toLocaleString("en-US",
-            {weekday: "short", month: "short", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric"}),
-            items: []
-            };
+        axios.get("/api/orders/user", {"headers": {"Authorization": "Bearer " + userInfo.token}})
+            .then((response) => {
+                console.log(response.data.orders);
+                response.data.orders.forEach(o => {
+                    var n = {
+                        id: o._id.toUpperCase(),
+                        cost: o.totalPrice,
+                        timestamp: "Purchased on: " + new Date(o.createdAt).toLocaleString("en-US",
+                            {
+                                weekday: "short",
+                                month: "short",
+                                day: "numeric",
+                                year: "numeric",
+                                hour: "numeric",
+                                minute: "numeric"
+                            }),
+                        items: []
+                    };
 
-            o.items.forEach(i =>
-            {
-                var s = ""
-                if (i.quantity > 1)
-                    s = "(" + i.quantity + ") " + i.name + " (" + i.quantity + " x " + price(i.price) + ")"
-                else
-                    s = i.name + " (" + price(i.price) + ")"
-                n.items.push(s);
-            })
+                    o.items.forEach(i => {
+                        var s = ""
+                        if (i.quantity > 1)
+                            s = "(" + i.quantity + ") " + i.name + " (" + i.quantity + " x " + price(i.price) + ")"
+                        else
+                            s = i.name + " (" + price(i.price) + ")"
+                        n.items.push(s);
+                    })
 
-            n.items.push("Shipping: " + price(o.shippingFee));
-            n.items.push("Taxes: " + price(o.taxPrice));
+                    n.items.push("Shipping: " + price(o.shippingFee));
+                    n.items.push("Taxes: " + price(o.taxPrice));
 
-            setOrders((old) => [...old, n]);
+                    setOrders((old) => [...old, n]);
+                });
             });
-        });
-    }, []);
+    }, [userInfo]);
 
     if (null === localStorage.getItem("token"))
         return <Navigate to="/"/>
 
-    var userData = JSON.parse(localStorage.getItem("token")).data;
+    // var userData = JSON.parse(localStorage.getItem("token")).data;
 
 
-    function logout(event)
-    {
-        localStorage.removeItem("token");
-        navigate("/")
-    }
+    // function logout(event)
+    // {
+    //     localStorage.removeItem("token");
+    //     navigate("/")
+    // }
 
-    function updateUser(event)
-    {
+    function updateUser(event) {
         event.preventDefault();
         lcupdate("");
         const u = Object.fromEntries(new FormData(event.currentTarget));
 
         console.log("Updating user with parameter: " + JSON.stringify(u));
 
-        axios.put("/api/users/account", u, {"headers": {"Authorization": "Bearer " + userData.token}})
+        axios.put("/api/users/account", u, {"headers": {"Authorization": "Bearer " + userInfo.token}})
         .then((response) =>
         {
             console.log(response.data);
@@ -104,75 +109,103 @@ export default function AccountManagement()
 
     return (
     <Container component="main" maxWidth="xs" style={{backgroundColor: "white", marginTop: "150px", minWidth: "45%"}}>
-    <Box sx={{ marginTop: 8, display: "flex", flexDirection: "column", alignItems: "center", }} >
+        <Box sx={{marginTop: 8, display: "flex", flexDirection: "column", alignItems: "center",}}>
 
-    <Typography component="h1" variant="h3" style={{color: "black", marginTop: "50px"}}> Account Info </Typography>
+            <Typography component="h1" variant="h3" style={{color: "black", marginTop: "50px"}}> Account
+                Info </Typography>
 
-    <Box
-    component="form" onSubmit={updateUser} sx={{
-    marginTop: 3, display: "flex", flexDirection: "row", alignItems: "center", width: "95%"}}>
-    <TextField
-    required fullWidth variant="outlined" InputProps={{style: {fontSize: "125%"}}} InputLabelProps={{style: {fontSize: "125%"}}}
-    name="username" autoFocus defaultValue={userData.username} label="Change Username"/>
-    <IconButton type="submit" color="inherit" sx={{backgroundColor: "darkgrey",transition: "background 0.5s, color 0.5s",
- ":hover": {backgroundColor: "black"}}}>
-        <EditIcon sx={{ color: grey[50] }} />
-    </IconButton>
-    </Box>
-    <Alert style={{ width: "90%", display: ((lastchanged === "username") ? 'block' : 'none') }} severity="success">You've successfully changed your username!</Alert>
-    <Alert style={{ width: "90%", display: ((lastchanged === "fail-username") ? 'block' : 'none') }} severity="error">Error - failed to change username.</Alert>
+            <Box
+                component="form" onSubmit={updateUser} sx={{
+                marginTop: 3, display: "flex", flexDirection: "row", alignItems: "center", width: "95%"
+            }}>
+                <TextField
+                    required fullWidth variant="outlined" InputProps={{style: {fontSize: "125%"}}}
+                    InputLabelProps={{style: {fontSize: "125%"}}}
+                    name="username" autoFocus defaultValue={userInfo.username} label="Change Username"/>
+                <IconButton type="submit" color="inherit" sx={{
+                    backgroundColor: "darkgrey", transition: "background 0.5s, color 0.5s",
+                    ":hover": {backgroundColor: "black"}
+                }}>
+                    <EditIcon sx={{color: grey[50]}}/>
+                </IconButton>
+            </Box>
+            <Alert style={{width: "90%", display: ((lastchanged === "username") ? 'block' : 'none')}}
+                   severity="success">You've
+                successfully changed your username!</Alert>
+            <Alert style={{width: "90%", display: ((lastchanged === "fail-username") ? 'block' : 'none')}}
+                   severity="error">Error - failed to change username.</Alert>
 
-    <Box
-    component="form" onSubmit={updateUser} sx={{
-    marginTop: 3, display: "flex", flexDirection: "row", alignItems: "center", width: "95%"}}>
-    <TextField required fullWidth variant="outlined" InputProps={{style: {fontSize: "125%"}}} InputLabelProps={{style: {fontSize: "125%"}}}
-    name="name" defaultValue={userData.name} label="Change Name"/>
-    <IconButton type="submit" color="inherit" sx={{backgroundColor: "darkgrey",transition: "background 0.5s, color 0.5s",
- ":hover": {backgroundColor: "black"}}}>
-        <EditIcon sx={{ color: grey[50] }} />
-    </IconButton>
-    </Box>
-    <Alert style={{ width: "90%", display: ((lastchanged === "name") ? 'block' : 'none') }} severity="success">You've successfully changed your name!</Alert>
-    <Alert style={{ width: "90%", display: ((lastchanged === "fail-name") ? 'block' : 'none') }} severity="error">Error - failed to change name.</Alert>
+            <Box
+                component="form" onSubmit={updateUser} sx={{
+                marginTop: 3, display: "flex", flexDirection: "row", alignItems: "center", width: "95%"
+            }}>
+                <TextField required fullWidth variant="outlined" InputProps={{style: {fontSize: "125%"}}}
+                           InputLabelProps={{style: {fontSize: "125%"}}}
+                           name="name" defaultValue={userInfo.name} label="Change Name"/>
+                <IconButton type="submit" color="inherit" sx={{
+                    backgroundColor: "darkgrey", transition: "background 0.5s, color 0.5s",
+                    ":hover": {backgroundColor: "black"}
+                }}>
+                    <EditIcon sx={{color: grey[50]}}/>
+                </IconButton>
+            </Box>
+            <Alert style={{width: "90%", display: ((lastchanged === "name") ? 'block' : 'none')}} severity="success">You've
+                successfully changed your name!</Alert>
+            <Alert style={{width: "90%", display: ((lastchanged === "fail-name") ? 'block' : 'none')}} severity="error">Error
+                - failed to change name.</Alert>
 
-    <Box
-    component="form" onSubmit={updateUser} sx={{
-    marginTop: 3, display: "flex", flexDirection: "row", alignItems: "center", width: "95%"}}>
-    <TextField required fullWidth variant="outlined" InputProps={{style: {fontSize: "125%"}}} InputLabelProps={{style: {fontSize: "125%"}}}
-    name="email" defaultValue={userData.email} label="Change Email"/>
-    <IconButton type="submit" color="inherit" sx={{backgroundColor: "darkgrey",transition: "background 0.5s, color 0.5s",
- ":hover": {backgroundColor: "black"}}}>
-        <EditIcon sx={{ color: grey[50] }} />
-    </IconButton>
-    </Box>
-    <Alert style={{ width: "90%", display: ((lastchanged === "email") ? 'block' : 'none') }} severity="success">You've successfully changed your email!</Alert>
-    <Alert style={{ width: "90%", display: ((lastchanged === "fail-email") ? 'block' : 'none') }} severity="error">Error - failed to change email address.</Alert>
+            <Box
+                component="form" onSubmit={updateUser} sx={{
+                marginTop: 3, display: "flex", flexDirection: "row", alignItems: "center", width: "95%"
+            }}>
+                <TextField required fullWidth variant="outlined" InputProps={{style: {fontSize: "125%"}}}
+                           InputLabelProps={{style: {fontSize: "125%"}}}
+                           name="email" defaultValue={userInfo.email} label="Change Email"/>
+                <IconButton type="submit" color="inherit" sx={{
+                    backgroundColor: "darkgrey", transition: "background 0.5s, color 0.5s",
+                    ":hover": {backgroundColor: "black"}
+                }}>
+                    <EditIcon sx={{color: grey[50]}}/>
+                </IconButton>
+            </Box>
+            <Alert style={{width: "90%", display: ((lastchanged === "email") ? 'block' : 'none')}} severity="success">You've
+                successfully changed your email!</Alert>
+            <Alert style={{width: "90%", display: ((lastchanged === "fail-email") ? 'block' : 'none')}}
+                   severity="error">Error - failed to change email address.</Alert>
 
-    <Box
-    component="form" onSubmit={updateUser} sx={{
-    marginTop: 3, display: "flex", flexDirection: "row", alignItems: "center", width: "95%"}}>
-    <TextField required fullWidth variant="outlined" InputProps={{style: {fontSize: "125%"}}} InputLabelProps={{style: {fontSize: "125%"}}}
-    name="password" label="Change Password"/>
-    <IconButton type="submit" color="inherit" sx={{backgroundColor: "darkgrey",transition: "background 0.5s, color 0.5s",
- ":hover": {backgroundColor: "black"}}}>
-        <EditIcon sx={{ color: grey[50] }} />
-    </IconButton>
-    </Box>
-    <Alert style={{ width: "90%", display: ((lastchanged === "password") ? 'block' : 'none') }} severity="success">You've successfully changed your password!</Alert>
-    <Alert style={{ width: "90%", display: ((lastchanged === "fail-password") ? 'block' : 'none') }} severity="error">Error - failed to change password.</Alert>
+            <Box
+                component="form" onSubmit={updateUser} sx={{
+                marginTop: 3, display: "flex", flexDirection: "row", alignItems: "center", width: "95%"
+            }}>
+                <TextField required fullWidth variant="outlined" InputProps={{style: {fontSize: "125%"}}}
+                           InputLabelProps={{style: {fontSize: "125%"}}}
+                           name="password" label="Change Password"/>
+                <IconButton type="submit" color="inherit" sx={{
+                    backgroundColor: "darkgrey", transition: "background 0.5s, color 0.5s",
+                    ":hover": {backgroundColor: "black"}
+                }}>
+                    <EditIcon sx={{color: grey[50]}}/>
+                </IconButton>
+            </Box>
+            <Alert style={{width: "90%", display: ((lastchanged === "password") ? 'block' : 'none')}}
+                   severity="success">You've successfully changed your password!</Alert>
+            <Alert style={{width: "90%", display: ((lastchanged === "fail-password") ? 'block' : 'none')}}
+                   severity="error">Error - failed to change password.</Alert>
 
-    <Box
-    sx={{ marginTop: 4, marginBottom: 2, alignItems: "center", width: "80%"}}>
-    <Button variant="contained" fullWidth color="error" onClick={logout}>Log Out</Button></Box>
+            {/*<Box*/}
+            {/*    sx={{marginTop: 4, marginBottom: 2, alignItems: "center", width: "80%"}}>*/}
+            {/*    <Button variant="contained" fullWidth color="error" onClick={logout}>Log Out</Button></Box>*/}
 
-    <Typography component="h1" variant="h3" style={{color: "black"}}> Your Orders ({orders.length}) </Typography>
+            <Typography component="h1" variant="h3" style={{color: "black"}}> Your Orders
+                ({orders.length}) </Typography>
 
-    <Grid container direction="column" spacing={4} alignItems="center" justifyContent="center" sx={{marginBottom: "50px"}}>
-    {orders.map((o) => {
-    return (
-    <Grid key={o.id} item sx={{ width: "100%" }}>
-        <Card sx={{backgroundColor: "#eeeeee"}}>
-        <CardContent>
+            <Grid container direction="column" spacing={4} alignItems="center" justifyContent="center"
+                  sx={{marginBottom: "50px"}}>
+                {orders.map((o) => {
+                    return (
+                        <Grid key={o.id} item sx={{width: "100%"}}>
+                            <Card sx={{backgroundColor: "#eeeeee"}}>
+                                <CardContent>
 
         <Typography variant="h5" color="text.secondary" gutterBottom>
             Order #{o.id}
