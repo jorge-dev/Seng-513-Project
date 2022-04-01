334d798771a366cb5016babbf1e9fe908e6cd2d8
jorge-dev <jo.luisa94@gmail.com>
2022-04-01 00:37:33 -0600

    added more customization to Message alert component

--- frontend/src/components/Demo/MessageAlert.jsx
+++ frontend/src/components/Demo/MessageAlert.jsx
@@ -3,7 +3,7 @@ import { Alert } from "react-bootstrap";
 export default function MessageAlert(props) {
     return (
 
-        <Alert className="text-center col-sm-12 mx-auto w-50" style={{ marginTop: '10em' }} variant={props.variant || 'info'}> {props.children} </Alert>
+        <Alert className="text-center col-sm-12 mx-auto w-50" style={props.custStyle || { marginTop: '10em' }} variant={props.variant || 'info'}> {props.children} </Alert>
 
     )
 }
