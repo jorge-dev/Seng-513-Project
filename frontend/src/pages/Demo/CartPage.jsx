742c6d1f6a90bb56de7a1c04fe9d2e708e180289
jorge-dev <jo.luisa94@gmail.com>
2022-04-01 11:01:44 -0600

    update the cart page with new changes

--- frontend/src/pages/Demo/CartPage.jsx
+++ frontend/src/pages/Demo/CartPage.jsx
@@ -1,21 +1,22 @@
-import { AddCircle, ArrowForward, Delete, RemoveCircle, RemoveCircleOutline } from "@mui/icons-material"
-import { Button, IconButton } from "@mui/material"
-import { useContext } from "react"
-import { Card, Col, Container, Image, ListGroup, Row } from "react-bootstrap"
-import { Helmet } from "react-helmet-async"
-import { red, grey } from '@mui/material/colors';
-import { Link, useNavigate } from "react-router-dom"
+import {AddCircle, ArrowForward, Delete, RemoveCircle, RemoveCircleOutline} from "@mui/icons-material"
+import {Button, IconButton} from "@mui/material"
+import {useContext} from "react"
+import {Card, Col, Container, Image, ListGroup, Row} from "react-bootstrap"
+import {Helmet} from "react-helmet-async"
+import {grey, red} from '@mui/material/colors';
+import {Link, useNavigate} from "react-router-dom"
 import MessageAlert from "../../components/Demo/MessageAlert"
-import { ContextStore } from "../../ContextStore"
+import {ContextStore} from "../../ContextStore"
 import "./styles/CartStyles.css"
+
 export default function CartPage() {
-    const { state: ctxState, setState: setCtxState } = useContext(ContextStore)
-    const { cart: { items } } = ctxState
+    const {state: ctxState, setState: setCtxState} = useContext(ContextStore)
+    const {cart: {items}} = ctxState
     const navigate = useNavigate()
 
     const updateCartHandler = (item, quantities) => {
         setCtxState({
-            type: "ADD_TO_CART", payload: { ...item, quantities }
+            type: "ADD_TO_CART", payload: {...item, quantities}
         });
     }
     const removeItemHandler = (item) => {
@@ -24,6 +25,10 @@ export default function CartPage() {
         });
     }
 
+    const checkoutHandler = () => {
+        navigate('/pages/Login?redirect=/shippingCart');
+    };
+
     const getSubtotal = () => {
         const subtotal = items.reduce((acc, item) => acc + item.price * item.quantities, 0)
         return Math.round(subtotal * 100) / 100
@@ -31,35 +36,38 @@ export default function CartPage() {
 
 
     return (
-        <div className="vh-100" style={{ marginTop: "8em" }}>
+        <div className="vh-100" style={{marginTop: "8em"}}>
             <Helmet>
                 <title>Shopping Cart</title>
             </Helmet>
             <h1 className="text-center">Shopping Cart</h1>
-            <Row style={{ margin: "0 20px" }} >
+            <Row style={{margin: "0 20px"}}>
                 <Col md={8}>
-                    {items.length === 0 ? (<MessageAlert custStyle={{ marginTop: '1.5em' }} ><h1>The Cart Is Empty</h1> <br />
-                        <Link style={{ color: 'black' }} to={'/'}>Checkout some of our featured products <ArrowForward /></Link>
-                    </MessageAlert>) : (
-                        <ListGroup style={{ background: '#252836', borderRadius: '30px' }}>
+                    {items.length === 0 ? (
+                        <MessageAlert custStyle={{marginTop: '1.5em'}}><h1>The Cart Is Empty</h1> <br/>
+                            <Link style={{color: 'black'}} to={'/'}>Checkout some of our featured
+                                products <ArrowForward/></Link>
+                        </MessageAlert>) : (
+                        <ListGroup style={{background: '#252836', borderRadius: '30px'}}>
                             {items.map(item => (
-                                <ListGroup.Item key={item._id} >
+                                <ListGroup.Item key={item._id}>
                                     <Row className="align-items-center">
                                         <Col md={7}>
-                                            <Image src={item.image} className=" mt-1 mb-1 mx-1 img-thumbnail" fluid />
+                                            <Image src={item.image} className=" mt-1 mb-1 mx-1 img-thumbnail" fluid/>
 
                                             {" "}
                                             <Link to={`/product/slug/${item.slug}`}>{item.name}</Link>
                                         </Col>
-                                        <Col md={2} style={{ color: 'white' }}>
-                                            <IconButton aria-label="add" onClick={() => updateCartHandler(item, item.quantities + 1)} >
-                                                <AddCircle sx={{ color: grey[50] }} />
+                                        <Col md={2} style={{color: 'white'}}>
+                                            <IconButton aria-label="add"
+                                                        onClick={() => updateCartHandler(item, item.quantities + 1)}>
+                                                <AddCircle sx={{color: grey[50]}}/>
                                             </IconButton> {" "}
                                             {item.quantities}
                                             {item.quantities === 1 ?
                                                 (
                                                     <IconButton aria-label="remove" disabled>
-                                                        <RemoveCircleOutline sx={{ color: grey[500] }} />
+                                                        <RemoveCircleOutline sx={{color: grey[500]}}/>
                                                     </IconButton>
                                                 ) :
                                                 (
@@ -114,7 +122,8 @@ export default function CartPage() {
                                 <ListGroup.Item className="border-bottom-0">
                                     <Container className="text-center d-grid">
                                         {items.length === 0 ? null :
-                                            <Button className="text-center align-middle" variant="contained" size="large" onClick={() => navigate('/signin?redirect=/shipping')}  >
+                                            <Button className="text-center align-middle" variant="contained"
+                                                    size="large" onClick={checkoutHandler}>
                                                 Proceed to Checkout
                                             </Button>
                                         }
