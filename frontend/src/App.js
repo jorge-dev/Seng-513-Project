a5a3132f72fbadc0a8d349c78ea599298ad14095
jorge-dev <jo.luisa94@gmail.com>
2022-04-01 23:49:03 -0600

    added the toastify package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,48 +7,63 @@ import CartPage from './pages/Demo/CartPage';
 import HomePage from './pages/Demo/HomePage';
 import ProductPage from './pages/Demo/ProductPage';
 
+
 import Login from './pages/Login'
 import CreateAccount from './pages/CreateAccount'
 import AccountManagement from './pages/AccountManagement'
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  const { state } = useContext(ContextStore)
-  const { cart } = state
+  const {state} = useContext(ContextStore)
+  const {cart} = state
   return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <div className=''>
-        <NavBar cart={cart} />
-        <main className="d-flex flex-column min-vh-100">
-          <Routes>
-            <Route path="/product/slug/:slug" element={<ProductPage />} />
-            <Route path="/" element={<HomePage />} />
-            <Route path="/shoppingCart" element={<CartPage />} />
+      <BrowserRouter>
+        <ScrollToTop/>
+        <ToastContainer
+            position="bottom-center"
+            // autoClose={2500}
+            // hideProgressBar={false}
+            // newestOnTop={false}
+            // closeOnClick
+            // rtl={false}
+            // pauseOnFocusLoss
+            // draggable
+            pauseOnHover
+            limit={1}
+        />
+        <div className=''>
+          <NavBar cart={cart}/>
+          <main className="d-flex flex-column min-vh-100">
+            <Routes>
+              <Route path="/product/slug/:slug" element={<ProductPage/>}/>
+              <Route path="/" element={<HomePage/>}/>
+              <Route path="/shoppingCart" element={<CartPage/>}/>
 
-            <Route path="pages/Login" element={<Login />}/>
-            <Route path="pages/CreateAccount" element={<CreateAccount/>}/>
-            <Route path="pages/AccountManagement" element={<AccountManagement/>}/>
-          </Routes>
+              <Route path="pages/Login" element={<Login/>}/>
+              <Route path="pages/CreateAccount" element={<CreateAccount/>}/>
+              <Route path="pages/AccountManagement" element={<AccountManagement/>}/>
+            </Routes>
 
-        </main>
-        <footer className='footer mt-4'>
-          <div className="text-center">All rights reserved &copy; 2020</div>
-          <div className="text-center"> Designed and implemented by: Ahmed Elnasri,Haotian Chen, Daryl Dang
-            , and Jorge Avila
-          </div>
-          <div className="text-center">All images are from the public domain and obtained from this websites: <br/>
-            <div className="h5">
-              <a href="https://oneofzero.net/">OneOfZero</a> {' '}
-              <a href="https://www.razer.com/ca-en/">Razer</a> {' '}
-              <a href="https://www.logitech.com/">Logitech</a>
+          </main>
+          <footer className='footer mt-4'>
+            <div className="text-center">All rights reserved &copy; 2020</div>
+            <div className="text-center"> Designed and implemented by: Ahmed Elnasri,Haotian Chen, Daryl Dang
+              , and Jorge Avila
             </div>
+            <div className="text-center">All images are from the public domain and obtained from this websites: <br/>
+              <div className="h5">
+                <a href="https://oneofzero.net/">OneOfZero</a> {' '}
+                <a href="https://www.razer.com/ca-en/">Razer</a> {' '}
+                <a href="https://www.logitech.com/">Logitech</a>
+              </div>
 
 
-          </div>
-        </footer>
+            </div>
+          </footer>
 
 
-      </div >
+        </div>
 
     </BrowserRouter >
   );
