7a4a6bda4f425594b9af198e9458412cd5dda995
jorge-dev <jo.luisa94@gmail.com>
2022-04-01 23:51:20 -0600

    added text formatting to error handler

--- frontend/src/utils/handleApiError.js
+++ frontend/src/utils/handleApiError.js
@@ -1,6 +1,10 @@
 export const getErrorMessage = (error) => {
     if (error.response && error.response.data) {
-        return error.response.data.message;
+
+        const errorStr = error.response.data.message;
+        //capitalize each word
+        return errorStr.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+
     }
     return error.message;
 }
