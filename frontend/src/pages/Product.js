4d80b711abe9f4460c63094b54f4660dec1f95b9
Daryl Dang <daryl.dang4@gmail.com>
2022-04-01 22:50:03 -0600

    Add remaining changes

--- frontend/src/pages/Product.js
+++ frontend/src/pages/Product.js
@@ -1,9 +1,55 @@
-import './styles/Product.css';
+import axios from 'axios';
+import { useContext, useEffect, useReducer, useRef, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { Navbar, Image, Col, Container, ListGroup, Row, Button, Badge, Card, Accordion } from 'react-bootstrap';
 import Rating from '../components/Rating'
 import ContextAwareToggle from '../components/ContextAwareToggle'
+import './styles/Product.css';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'REFRESH_PRODUCT':
+      return { ...state, product: action.payload };
+    case 'CREATE_REQUEST':
+      return { ...state, loadingCreateReview: true };
+    case 'CREATE_SUCCESS':
+      return { ...state, loadingCreateReview: false };
+    case 'CREATE_FAIL':
+      return { ...state, loadingCreateReview: false };
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, product: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 export default function Product() {
+  const params = useParams();
+  const { slug } = params;
+
+  const [{ loading, error, product, loadingCreateReview }, dispatch] =
+    useReducer(reducer, {
+      product: [],
+      loading: true,
+      error: '',
+    });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get(`/api/products/slug/${slug}`);
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
+    };
+    fetchData();
+  }, [slug]);
   return (
       <>
         { /* Product page */}
