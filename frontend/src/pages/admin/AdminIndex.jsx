8d2235aefd6cb925d09eade2b818273f4499100d
Haotian Chen <47372380+haot1anchen@users.noreply.github.com>
2022-04-03 04:13:53 -0600

    Update AdminIndex.jsx

--- frontend/src/pages/admin/AdminIndex.jsx
+++ frontend/src/pages/admin/AdminIndex.jsx
@@ -7,4 +7,29 @@ import '../styles/Admin.css';
 
 function AdminIndex() {
     let [currentNavIndex, setCurrentNavIndex] = useState(0);
+    
+    return(
+        <div className="adminBox">
+            <div className="adminHeader">
+                <div className="adminHeaderCenter">
+                    <div className="adminHeaderTit">
+                        <img className="logo" src={fullLogo} alt="" srcset="" />
+                    </div>
+
+                    <div className="adminNavBox">
+                        {['Users', 'Products', 'Transactions'].map((v, i) => {
+                            return(
+                                <div key={i} onClick={() => {
+                                    setCurrentNavIndex(i);
+                                }} className={`adminNav ${currentNavIndex === i ? 'adminActiveNav' : ''}`}>
+                                    {v}
+                                    </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
 }
