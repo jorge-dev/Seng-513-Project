79556c9b61eb068f52a1664bf7fbf984a812c952
Haotian Chen <47372380+haot1anchen@users.noreply.github.com>
2022-04-03 03:56:51 -0600

    Create AdminIndex.jsx

---
+++ frontend/src/pages/admin/AdminIndex.jsx
@@ -0,0 +1,6 @@
+import { useState, useEffect, useReducer } from "react";
+import AdminProducts from './AdminProducts';
+import AdminUsers from './AdminUsers';
+import AdminTrans from './AdminTrans';
+import fullLogo from '../../logos/fullLogo.png';
+import '../styles/Admin.css';
