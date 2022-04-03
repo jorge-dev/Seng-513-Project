5e264f7f1d0acaa8278176a627ded4779e36bf4b
Haotian Chen <47372380+haot1anchen@users.noreply.github.com>
2022-04-03 06:04:42 -0600

    Update AdminProducts.jsx
    bug fix

--- frontend/src/pages/admin/AdminProducts.jsx
+++ frontend/src/pages/admin/AdminProducts.jsx
@@ -1,14 +1,14 @@
-import { useState, useEffect, useReducer } from "react"; 
-import {Table, Modal, Button, Form} from 'react-bootstrap';
-import axios from 'axios';
+import { useState, useEffect, useReducer } from "react";
+import { Table, Modal, Button, Form } from 'react-bootstrap';
+import axios from "axios";
 import '../styles/AdminProducts.css';
 
 function AdminProducts() {
     const [show, setShow] = useState(false);
     const [currentOperation, setCurrentOperation] = useState('ADD');
     const [currentItem, setCurrentItem] = useState(null);
-    const [tabledata, setTabledata] = useState([]);
-    const [formdata, setFormdata] = useState({
+    const [tableData, setTableData] = useState([]);
+    const [formModel, setFormModel] = useState({
         name: '',
         vendor: '',
         price: '',
@@ -19,30 +19,30 @@ function AdminProducts() {
         inStock: 'true',
         rating: '1',
         numberOfReviews: ''
-});
+    });
 
-const handleClose = () => setShow(false);
+    const handleClose = () => setShow(false);
 
-const handleSubmit = () => {
-    if (currentOperation == 'ADD') {
-        // add product
-        axios.post('/api/products', formModel).then((res) => {
-            alert(res.data.message)
-            setShow(false)
-            getTableData()
-        }).catch((err) => {
-            console.log("err = ", err)
-            alert('Error')
-        })
-    } else {
-        // edit product
-        axios.put(`/api/products/${currentItem._id}`, formModel).then((res) => {
-            alert(res.data.message)
-            setShow(false)
-            getTableData()
-        }).catch((err) => {
-            console.log("err = ", err)
-            alert('Error')
-        })
+    const handleSubmit = () => {
+        if (currentOperation == 'ADD') {
+            // add product
+            axios.post('/api/products', formModel).then((res) => {
+                alert(res.data.message)
+                setShow(false)
+                getTableData()
+            }).catch((err) => {
+                console.log("err = ", err)
+                alert('Error')
+            })
+        } else {
+            // edit product
+            axios.put(`/api/products/${currentItem._id}`, formModel).then((res) => {
+                alert(res.data.message)
+                setShow(false)
+                getTableData()
+            }).catch((err) => {
+                console.log("err = ", err)
+                alert('Error')
+            })
+        }
     }
-}
