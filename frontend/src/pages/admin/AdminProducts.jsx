d3b8bcc3605eb1c891feb4a92752cee4219f38ae
Haotian Chen <47372380+haot1anchen@users.noreply.github.com>
2022-04-03 05:53:22 -0600

    Create AdminProducts.jsx

---
+++ frontend/src/pages/admin/AdminProducts.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect, useReducer } from "react"; 
+import {Table, Modal, Button, Form} from 'react-bootstrap';
+import axios from 'axios';
+import '../styles/AdminProducts.css';
+
+function AdminProducts() {
+    const [show, setShow] = useState(false);
+    const [currentOperation, setCurrentOperation] = useState('ADD');
+    const [currentItem, setCurrentItem] = useState(null);
+    const [tabledata, setTabledata] = useState([]);
+    const [formdata, setFormdata] = useState({
+        name: '',
+        vendor: '',
+        price: '',
+        description: '',
+        image: '',
+        mainCategory: '',
+        subCategory: '',
+        inStock: 'true',
+        rating: '1',
+        numberOfReviews: ''
+});
+
+const handleClose = () => setShow(false);
+
+const handleSubmit = () => {
+    if (currentOperation) == 'ADD') {
+        //add products
+        axios.post('/api/products', formModel).then((res) => {
+            alert(res.data.message)
+            setShow(false);
+            getTableData()
+        }).catch((err) => {
+            console.log("err = ", err)
+            alert('Error')
+        })
+    }
+}
