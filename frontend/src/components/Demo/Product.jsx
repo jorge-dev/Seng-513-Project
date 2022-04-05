ec66a5b5938c44e5b1bdfa3a16b9e7a43711e76f
jorge-dev <jo.luisa94@gmail.com>
2022-04-05 01:48:20 -0600

    Fixed item on sale bug

--- frontend/src/components/Demo/Product.jsx
+++ frontend/src/components/Demo/Product.jsx
@@ -13,16 +13,16 @@ import { ContextStore } from "../../ContextStore";
 
 
 function applyDiscount(price, discount) {
-    const discounted = price - (price * discount / 100);
+    const discounted = price + (price * discount / 100);
     return Math.round(discounted * 100) / 100;
 }
 
 
 
 function Product(props) {
-    const { product, loading, onSale, discountPercent } = props;
-    const { state: ctxState, setState: setCtxState } = useContext(ContextStore)
-    const { cart: { items } } = ctxState
+    const {product, loading, onSale, discountPercent} = props;
+    const {state: ctxState, setState: setCtxState} = useContext(ContextStore)
+    const {cart: {items}} = ctxState
     // const navigate = useNavigate()
 
     const addToCartHandler = (item) => {
@@ -39,35 +39,38 @@ function Product(props) {
             {loading ? <Skeleton variant="rect" width={'100%'} height={'10em'} /> :
                 <Link to={`/product/slug/${product.slug}`}>
                     {onSale ?
-                        <Badge bg="danger" style={{ position: 'absolute', top: '10px', left: '10px', fontSize: ".8em" }}>
-                            Sale
+                        <Badge bg="danger" style={{position: 'absolute', top: '10px', left: '10px', fontSize: ".8em"}}>
+                            {discountPercent}% Off
                         </Badge> : null
                     }
                     <Card.Img variant="top" alt="product-image" style={{ height: '10em', width: '100%', background: '#d1d1d1', borderRadius: "30px 30px 0 0" }} src={product.image} />
                 </Link>
             }
             <Card.Body className="d-flex flex-column mt-auto">
-                {loading ? <Skeleton variant="text" width={'100%'} height={'100%'} /> :
+                {loading ? <Skeleton variant="text" width={'100%'} height={'100%'}/> :
                     <Link to={`/product/slug/${product.slug}`}>
-                        <Card.Title className="mt-auto" style={{ color: 'black', fontSize: '1em' }}>{product.name}</Card.Title>
+                        <Card.Title className="mt-auto"
+                                    style={{color: 'black', fontSize: '1em'}}>{product.name}</Card.Title>
                     </Link>
                 }
 
-                {loading ? <Skeleton variant="text" width={'100%'} height={'100%'} /> :
-                    < div className="mt-auto" >
-                        <Ratings align="left" ratingReceived={product.rating} numberOfReviews={product.numberOfReviews} readOnly={true} isDark={false} />
+                {loading ? <Skeleton variant="text" width={'100%'} height={'100%'}/> :
+                    < div className="mt-auto">
+                        <Ratings showReviews={true} align="left" ratingReceived={product.rating}
+                                 numberOfReviews={product.numberOfReviews} readOnly={true} isDark={false}/>
                     </div>
                 }
-                {loading ? <Skeleton variant="text" width={'100%'} height={'100%'} /> :
-                    < div className="mt-auto" style={{ color: 'black' }}>
+                {loading ? <Skeleton variant="text" width={'100%'} height={'100%'}/> :
+                    < div className="mt-auto" style={{color: 'black'}}>
 
                         {onSale ?
                             <Row>
-                                <Col sm={4} style={{ paddingRight: "0" }} >
-                                    <span className="text-muted" style={{ textDecoration: 'line-through' }}>${product.price}</span>
+                                <Col sm={4} style={{paddingRight: "0"}}>
+                                    <span className="text-muted"
+                                          style={{textDecoration: 'line-through'}}>${applyDiscount(product.price, discountPercent)}</span>
                                 </Col>
-                                <Col sm={8} style={{ paddingLeft: "0" }}>
-                                    <p className="text-success" style={{ fontWeight: 'bold' }}>${applyDiscount(product.price, discountPercent)}</p>
+                                <Col sm={8} style={{paddingLeft: "0"}}>
+                                    <p className="text-success" style={{fontWeight: 'bold'}}>${product.price}</p>
                                 </Col>
                             </Row>
                             : product.price}
